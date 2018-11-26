6546c420bc253c1396d73cc8b3bfccb3c892738f
Raphael Katana <roduor41@gmail.com>
2018-11-27 00:58:30 +0300

    added models for comments and blogs

--- app/models.py
+++ app/models.py
@@ -6,7 +6,7 @@ from werkzeug.security import generate_password_hash,check_password_hash
 class User(UserMixin,db.Model):
     __tablename__='users'
     id=db.Column(db.Integer,primary_key=True)
-    username=db.Column(db.Strin(150),index=True)
+    username=db.Column(db.String(150),index=True)
     email=db.Column(db.String(150),unique=True,index=True)
     pass_secure=db.Column(db.String(100))
     blogs=db.relationship('Blogs',backref='user',lazy='dynamic')
@@ -30,6 +30,12 @@ class Blogs(db.Model):
     title=db.Column(db.String(100))
     description=db.Column(db.String(250))
     user_id=db.Column(db.Integer,db.ForeignKey('users.id'))
+
+    @classmethod
+    def delete_blog(cls,id):
+        blog=Blogs.query.filter_by(id=id).first()
+        db.session.delete(blog)
+        db.session.commit
 
 class Comments(db.Model):
     __tablename__='comments'
