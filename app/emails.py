5e7e17dee110ef069ed0cd2e9f929fdacffec74d
Raphael Katana <roduor41@gmail.com>
2018-11-27 01:01:40 +0300

    styled the application

--- app/emails.py
+++
@@ -1,8 +0,0 @@
-from .import mail
-from flask_mail import Message
-from flask import render_template
-
-def mail_message(subject,template,to,**kwargs):
-    sender_email='roduor52@gmail.com'
-    email=Message(subject,sender=sender_email,recipients=[to])
-    email.body=render_template(template + '.txt',**kwargs)
